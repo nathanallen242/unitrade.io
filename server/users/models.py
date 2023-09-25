b3edbca692efcbc5921b5150294e5349ffc76a67
nate <70911747+nathanallen242@users.noreply.github.com>
2023-09-25 11:55:23 -0400

    user id fix

--- server/users/models.py
+++ server/users/models.py
@@ -7,7 +7,7 @@ from .. import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
